3ce5f31327de4db46792abba3cbe74821eb04d01
NicholasAbri <nicholaskofiabri@gmail.com>
2026-09-01 16:50:42 +0000

    Update project

--- FRONTEND/app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const server = express();
-
-const port = 4000;
-
-server.use(port, "localhost", () => {
-  console.log(`Server is running on port ${port}`);
-});
